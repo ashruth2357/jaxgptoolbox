8688885470b2b0b6d42a6039b85ae504adfbc0e1
ashruth2357 <137484685+ashruth2357@users.noreply.github.com>
2023-11-02 16:34:33 +0530

    Update model.py

--- demos/lipschitz_mlp/model.py
+++ demos/lipschitz_mlp/model.py
@@ -106,7 +106,7 @@ class lipmlp:
     (Optional) this is a standard forward pass of a mlp. This is useful to speed up the performance during test time 
     """
     # concatenate coordinate and latent code
-    x = np.append(x,t)
+    x = np.append(x,[t,t+1])
 
 
     # forward pass
